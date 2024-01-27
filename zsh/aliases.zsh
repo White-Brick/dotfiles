3f2b68d67b84546188701bb76afb0e26d95a8c15
GeeWinter <815672122@qq.com>
2024-01-27 13:53:03 +0800

    update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,7 +9,7 @@ alias delELF='find . -maxdepth 1 -type f -perm +111 -exec file {} \; | grep "exe
 # File path alias
 alias Code='cd ~/Code'
 alias Notes='cd /Users/geewinter/Library/Mobile\ Documents/com\~apple\~CloudDocs/Coding/Notes/'
-alias algorithm='cd /Users/geewinter/Code/c++/study/algorithm'
+alias algorithm='cd /Users/geewinter/Code/02_c++/study/algorithm'
 alias config='cd ~/.config'
 alias Linux='cd ~/Code/02_C++/Linux/'
 
