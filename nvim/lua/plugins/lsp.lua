e06f4529e951bd706dc1a4a5229b474f8d8f89a0
GeeWinter <815672122@qq.com>
2024-08-15 03:29:18 +0800

    Try to let clangd support c++20, but failed

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -12,46 +12,6 @@ return {
       })
     end,
   },
-  -- -- Set up lspconfig.
-  -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-  -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  -- require("lspconfig")["clangd"].setup({
-  --   capabilities = capabilities,
-  -- })
-
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = function(_, opts)
-  --     -- Add additional capabilities supported by nvim-cmp
-  --     -- local on_attach = require("plugins.configs.lspconfig").on_attach
-  --     -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-  --     local base = require("cmp_nvim_lsp")
-  --     local on_attach = base.on_attach
-  --     local capabilities = base.capabilities
-
-  --     local lspconfig = require("lspconfig")
-
-  --     -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-  --     local servers = {
-  --       "clangd",
-  --       -- 'rust_analyzer',
-  --       -- 'pyright',
-  --       -- 'tsserver'
-  --     }
-
-  --     for _, lsp in ipairs(servers) do
-  --       lspconfig[lsp].setup({
-  --         -- on_attach = my_custom_on_attach,
-  --         on_attach = function(client, bufnr)
-  --           client.server_capabilities.signatureHelpProvider = true
-  --           on_attach(client, bufnr)
-  --         end,
-  --         capabilities = capabilities,
-  --       })
-  --     end
-  --   end,
-  -- },
-
   -- lsp servers
   {
     "neovim/nvim-lspconfig",
@@ -59,7 +19,17 @@ return {
       -- inlay_hints = { enabled = true },
       -- ---@type lspconfig.options
       servers = {
-        clangd = {},
+        clangd = {
+          -- cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
+          --
+          -- initialization_options = {
+          --   fallback_flags = { "-std=c++20" },
+          -- },
+          --
+          -- keys = {
+          --   { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+          -- },
+        },
         pyright = {},
         -- gopls = {},
       },
